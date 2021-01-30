66a661f8a2f60bb1d685d36080b975a0ed65464a
AyushDash50 <71762392+AyushDash50@users.noreply.github.com>
2021-01-30 12:27:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig3;
 var backgroundImg,platform;
+var bird, slingshot;
+var score = 0;
+
+var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getbackground();
+    
 }
 
 function setup(){
@@ -17,7 +23,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 300, 300, 170);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,25 +40,35 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    
 }
 
 function draw(){
+    if (backgroundImg) {    
     background(backgroundImg);
+    }
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
+    textSize(30);
+    fill("blue");
+    text("Score:  "+ score,1000,50);
+    
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -61,4 +77,47 @@ function draw(){
 
     bird.display();
     platform.display();
+    //log6.display();
+    slingshot.display();    
 }
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32 && bird.body.speed<1){
+        Matter.Body.setPosition(bird.body, {x:200,y:50});
+        slingshot.attach(bird.body);
+        bird.trajectory = []
+        gameState = 'onSling'
+    }
+}
+
+async function getbackground(){
+
+    var response =  await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responsejson =  await response.json();
+
+    var datime = responsejson.datetime;
+    var hour = datime.slice(11,13);
+
+    console.log(hour);
+
+    if(hour>=06 & hour<=19 ){
+
+        bg = "sprites/bg.png"
+    }else {
+        bg = "sprites/bg2.jpg" 
+    }
+    backgroundImg = loadImage(bg);
+
+}       
